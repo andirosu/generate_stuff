740c12b49807dee54e24851e1f1492719c8cecc8
Andrei Rosu <30288530+andirosu@users.noreply.github.com>
2021-09-23 20:41:02 +0300

    init

---
+++ generate_products.php
@@ -0,0 +1,35 @@
+<?php
+use Magento\Framework\App\Bootstrap;
+include('../app/bootstrap.php');
+$bootstrap = Bootstrap::create(BP, $_SERVER);
+
+$objectManager = $bootstrap->getObjectManager();
+
+$state = $objectManager->get('Magento\Framework\App\State');
+$state->setAreaCode('frontend');
+
+for($i=1;$i<=30;$i++) {
+
+    $_product = $objectManager->create('Magento\Catalog\Model\Product');
+    $_product->setName("Produs Test nr. $i");
+    $_product->setTypeId('simple');
+    $_product->setAttributeSetId(4);
+    $_product->setSku('"prod-test-$i"');
+    $_product->setWebsiteIds([1]);
+    $_product->setVisibility(4);
+    $_product->setPrice([1]);
+    $_product->setImage('/img/1000x1000.png');
+    $_product->setSmallImage('/img/1000x1000.png');
+    $_product->setThumbnail('/img/1000x100.png');
+    $_product->setStockData([
+            'use_config_manage_stock' => 0, //'Use config settings' checkbox
+            'manage_stock' => 1, //manage stock
+            'min_sale_qty' => 1, //Minimum Qty Allowed in Shopping Cart
+            'max_sale_qty' => 100, //Maximum Qty Allowed in Shopping Cart
+            'is_in_stock' => 1, //Stock Availability
+            'qty' => 100 //qty
+        ]
+    );
+    $_product->save();
+}
+?>
